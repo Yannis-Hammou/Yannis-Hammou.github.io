e7b31a5f19c278c52cdf08e63f24e7807c99e806
DAH 08 <dah08@FORMATION.lan>
2020-02-07 11:32:59 +0100

    premier commit

---
+++ precache-manifest.9a152715ec3180d00a7c852bd7717374.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd947b1530d7ddcaa0ba82f0bfdc9612",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fddde70f43155de74b6",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ec1a8d0391108b462c9d",
+    "url": "/static/js/2.ac713011.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.ac713011.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fddde70f43155de74b6",
+    "url": "/static/js/main.45d32106.chunk.js"
+  },
+  {
+    "revision": "f1cf395d4096cf2bc3b3",
+    "url": "/static/js/runtime-main.e4579682.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
